fe2b66461915e6799af86b501c8362d2149bde3d
Randy Bielby <rjbielby@madisoncollege.edu>
2019-02-04 16:49:40 -0600

    Adding ConditionalDemo

---
+++ projects/src/java111/project1/demos/ConditionalDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Demonstrating if/else statements and booleans
+ *
+ * @author rjbielby
+ */
+public class ConditionalDemo {
+
+    /**
+     * create a variable and then use if/else checks
+     * to perform displays of messages
+     */
+    public static void main(String[] args) {
+
+        int myNumber = 50;
+
+        if (myNumber == 50) {
+            System.out.println("The number is 50... " + myNumber);
+        } else if (myNumber == 30) {
+                System.out.println("The number is 30.. " + myNumber);
+        } else {
+            System.out.println("The number not 30 or 50. It is " + myNumber);
+        }
+    }
+
+}
