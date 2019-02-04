f3b31a326e4e2a96ed0702f451c6a48f99846c09
Randy Bielby <rjbielby@madisoncollege.edu>
2019-02-04 16:38:19 -0600

    Demos from day 1 videos

---
+++ projects/src/java111/project1/demos/AssignmentDemo.java
@@ -0,0 +1,20 @@
+/**
+ * Program javadoc goes here.  Tell what the class is/does
+ *
+ * @author rjbielby
+ */
+public class AssignmentDemo {
+
+    /**
+     * Create an in variable and a String variable, assign
+     * values to them, and then display them
+     */
+     public static void main(String[] args) {
+         int myNumber = 123;
+         String myString = "This my string";
+
+         System.out.println("This is my number variable " + myNumber);
+         System.out.println("This is my string variable " + myString);
+     }
+
+}
---
+++ projects/src/java111/project1/demos/MyFirstApp.java
@@ -0,0 +1,16 @@
+/**
+ * Demonstrates a println and main method
+ * this is the first class we are writing this semester
+ *
+ * @author rjbielby
+ */
+public class MyFirstApp {
+
+    /**
+     * This main method writes out some text to the console
+     */
+    public static void main(String[] args) {
+        System.out.println("This is the first class I've written");
+    }
+
+}
